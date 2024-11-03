f011a3f4feeb1d2baa317ab121484787cb6ac7e1
Gabhyxx <jgabrielpg00@gmail.com>
2024-11-03 11:01:51 +0100

    JSON + Links + Cards + Cards Style

--- js/index.js
+++ js/index.js
@@ -13,22 +13,21 @@ async function fetchGamesJson() {
   }
 }
 
-function createGameCard({ id, image, price, platform, descrition }) {
+function createGameCard({ id, name, image, price, platform, description }) {
   return `
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+        <div class="card" style="width: 35rem;">
+            <img src="${image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title">${id} - ${name}</h5>
+                <p class="card-text">${description}</p>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">${price} €</li>
+                <li class="list-group-item">${platform}</li>
             </ul>
             <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <a href="#" class="card-link">Buy</a>
+                <a href="#" class="card-link">Wishlist</a>
             </div>
         </div>
     `;
